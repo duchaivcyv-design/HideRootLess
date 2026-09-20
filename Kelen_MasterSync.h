ba4efac96a3e296df59cd1e7696bf0d50e721ff4
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-20 22:26:10 +0700

    Create Kelen_MasterSync.h

---
+++ Kelen_MasterSync.h
@@ -0,0 +1,33 @@
+#import <Foundation/Foundation.h>
+#import <dlfcn.h>
+#import <sys/stat.h>
+#import <sys/sysctl.h>
+#import <mach-o/dyld.h>
+#import <errno.h>
+#import <spawn.h>
+#import <objc/runtime.h>
+
+#define KELEN_PREFS_PATH @"/var/mobile/Library/Preferences/com.kelen.deepbypass.plist"
+#define KELEN_LOG(fmt, ...) NSLog(@"[KelenDeepBypass] " fmt, ##__VA_ARGS__)
+
+// Cấu trúc cấu hình đồng bộ cho từng ứng dụng mục tiêu
+typedef struct {
+    BOOL isEnabled;
+    BOOL hookFileSystem;
+    BOOL hookDyldImages;
+    BOOL hookSyscallCore;
+    BOOL hookAntiDebug;
+    BOOL hookSandboxVirtual;
+    BOOL hookObjCRuntime;
+} KelenAppConfig;
+
+// Khai báo các hàm đồng bộ trung tâm
+#cplusplus
+extern "C" {
+#cplusplus
+    BOOL Kelen_ShouldBypassCurrentApp(void);
+    KelenAppConfig Kelen_GetAppConfig(void);
+    void Kelen_InitializeAllModules(void);
+#cplusplus
+}
+#cplusplus
