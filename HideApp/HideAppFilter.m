c9370be75eb66a29fcbced2b578aa3755d42af6a
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-21 03:54:42 +0700

    Update HideAppFilter.m

--- HideApp/HideAppFilter.m
+++ HideApp/HideAppFilter.m
@@ -1,3 +1,4 @@
+#import <Foundation/Foundation.h>
 #import "../Kelen_MasterSync.h"
 
 __attribute__((constructor)) static void HideAppProcessFilterEntry(void) {
@@ -8,7 +9,8 @@ __attribute__((constructor)) static void HideAppProcessFilterEntry(void) {
         if (bundleID) {
             NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:KELEN_PREFS_PATH];
             if (prefs && [prefs[bundleID] boolValue]) {
-                KELEN_LOG:@"[HideApp] Tiến trình mục tiêu [%@] đã được bật cơ chế ẩn hoàn toàn!", bundleID;
+                // Đã chuẩn hóa lại hàm NSLog/macro log đúng cú pháp
+                NSLog(@"[HideApp] Tiến trình mục tiêu [%@] đã được bật cơ chế ẩn hoàn toàn!", bundleID);
                 // Thực hiện gắn cờ hoặc kích hoạt các móc nối bảo mật chuyên sâu riêng cho app này
             }
         }
