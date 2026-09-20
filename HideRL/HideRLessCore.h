9d781eeaceca64655559a6429db7e4d0503a06fb
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-21 02:19:08 +0700

    Update HideRLessCore.h

--- HideRL/HideRLessCore.h
+++ HideRL/HideRLessCore.h
@@ -1,81 +1,26 @@
-#import "HideRLessCore.h"
-#import <notify.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-#define KELEN_MASTER_PREFS KELEN_MASTER_PREFS_PATH
+#define KELEN_MASTER_PREFS_PATH @"/var/mobile/Library/Preferences/com.kelen.masterbypass.plist"
+#define KELEN_NOTIFICATION_RELOAD CFSTR("com.kelen.masterbypass/ReloadPrefs")
 
-@implementation HideRLessCore {
-    NSMutableDictionary *_cachedPreferences;
-}
+NS_ASSUME_NONNULL_BEGIN
 
-+ (instancetype)sharedInstance {
-    static HideRLessCore *sharedInstance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        sharedInstance = [[self alloc] init];
-    });
-    return sharedInstance;
-}
+@interface HideRLessCore : NSObject
 
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        [self loadPreferences];
-        
-        CFNotificationCenterAddObserver(
-            CFNotificationCenterGetDarwinNotifyCenter(),
-            (__bridge const void *)(self),
-            (CFNotificationCallback)&GlobalPreferencesChangedCallback,
-            KELEN_NOTIFICATION_RELOAD,
-            NULL,
-            CFNotificationSuspensionBehaviorDeliverImmediately
-        );
-    }
-    return self;
-}
-
-static void GlobalPreferencesChangedCallback(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
-    [[HideRLessCore sharedInstance] loadPreferences];
-}
-
-- (void)loadPreferences {
-    @autoreleasepool {
-        NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:KELEN_MASTER_PREFS];
-        if (prefs) {
-            _cachedPreferences = [prefs mutableCopy];
-        } else {
-            _cachedPreferences = [NSMutableDictionary dictionary];
-        }
-    }
-}
-
-- (BOOL)isGlobalBypassEnabled {
-    @autoreleasepool {
-        id val = _cachedPreferences[@"KelenMasterSwitch"];
-        return val ? [val boolValue] : YES;
-    }
-}
-
-- (BOOL)isFeatureActiveForKey:(NSString *)key {
-    if (![self isGlobalBypassEnabled]) {
-        return NO;
-    }
-    if (!key) {
-        return YES;
-    }
-    @autoreleasepool {
-        id val = _cachedPreferences[key];
-        return val ? [val boolValue] : YES;
-    }
-}
-
-- (void)postReloadNotification {
-    CFNotificationCenterPostNotification(
-        CFNotificationCenterGetDarwinNotifyCenter(),
-        KELEN_NOTIFICATION_RELOAD,
-        NULL,
-        NULL,
-        YES
-    );
-}
++ (instancetype)sharedInstance;
+- (BOOL)isGlobalBypassEnabled;
+- (BOOL)isFeatureActiveForKey:(NSString *)key;
+- (void)postReloadNotification;
 
 @end
+
+@protocol HideRLessModuleProtocol <NSObject>
+@required
+- (BOOL)isModuleEnabled;
+- (void)registerHooks;
+@optional
+- (void)reloadModuleConfiguration;
+@end
+
+NS_ASSUME_NONNULL_END
