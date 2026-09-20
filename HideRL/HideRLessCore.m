42b954e8561970c1e9a27763be02c92b381c14d8
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-21 02:19:52 +0700

    Update HideRLessCore.m

--- HideRL/HideRLessCore.m
+++ HideRL/HideRLessCore.m
@@ -1,6 +1,11 @@
 #import "HideRLessCore.h"
+#import <notify.h>
 
-@implementation HideRLessCore
+#define KELEN_MASTER_PREFS KELEN_MASTER_PREFS_PATH
+
+@implementation HideRLessCore {
+    NSMutableDictionary *_cachedPreferences;
+}
 
 + (instancetype)sharedInstance {
     static HideRLessCore *sharedInstance = nil;
@@ -11,12 +16,66 @@
     return sharedInstance;
 }
 
-- (BOOL)isMasterSwitchEnabled {
-    NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:KELEN_MASTER_PREFS];
-    if (prefs && prefs[@"KelenMasterSwitch"]) {
-        return [prefs[@"KelenMasterSwitch"] boolValue];
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self loadPreferences];
+        
+        CFNotificationCenterAddObserver(
+            CFNotificationCenterGetDarwinNotifyCenter(),
+            (__bridge const void *)(self),
+            (CFNotificationCallback)&GlobalPreferencesChangedCallback,
+            KELEN_NOTIFICATION_RELOAD,
+            NULL,
+            CFNotificationSuspensionBehaviorDeliverImmediately
+        );
     }
-    return YES;
+    return self;
+}
+
+static void GlobalPreferencesChangedCallback(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
+    [[HideRLessCore sharedInstance] loadPreferences];
+}
+
+- (void)loadPreferences {
+    @autoreleasepool {
+        NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:KELEN_MASTER_PREFS];
+        if (prefs) {
+            _cachedPreferences = [prefs mutableCopy];
+        } else {
+            _cachedPreferences = [NSMutableDictionary dictionary];
+        }
+    }
+}
+
+- (BOOL)isGlobalBypassEnabled {
+    @autoreleasepool {
+        id val = _cachedPreferences[@"KelenMasterSwitch"];
+        return val ? [val boolValue] : YES;
+    }
+}
+
+- (BOOL)isFeatureActiveForKey:(NSString *)key {
+    if (![self isGlobalBypassEnabled]) {
+        return NO;
+    }
+    if (!key) {
+        return YES;
+    }
+    @autoreleasepool {
+        id val = _cachedPreferences[key];
+        return val ? [val boolValue] : YES;
+    }
+}
+
+- (void)postReloadNotification {
+    CFNotificationCenterPostNotification(
+        CFNotificationCenterGetDarwinNotifyCenter(),
+        KELEN_NOTIFICATION_RELOAD,
+        NULL,
+        NULL,
+        YES
+    );
 }
 
 @end
