8c70fea879c6874faf8f19354a4795d5395f97d2
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-23 16:14:46 +0700

    Update KelenAdvancedSubController.m

--- KelenCore/KelenAdvancedSubController.m
+++ KelenCore/KelenAdvancedSubController.m
@@ -1,11 +1,8 @@
 #import <UIKit/UIKit.h>
-#import <spawn.h>
+#import <objc/runtime.h>
 #import "Kelen_MasterSync.h"
 
 // --- KHAI BÁO GIẢ (FORWARD DECLARATION) CHO PREFERENCES FRAMEWORK ---
-typedef id PSSpecifierType;
-typedef id PSEditPaneType;
-
 @interface PSSpecifier : NSObject
 + (instancetype)preferenceSpecifierNamed:(NSString *)title target:(id)target set:(SEL)set get:(SEL)get detail:(Class)detail cell:(int)cell edit:(Class)edit;
 - (void)setProperty:(id)value forKey:(NSString *)key;
@@ -18,247 +15,126 @@ typedef id PSEditPaneType;
 @property (nonatomic, strong) NSArray *specifiers;
 @end
 
-// Các hằng số cell type của Preferences framework
 static const int PSGroupCell = 0;
 static const int PSSwitchCell = 2;
-static const int PSButtonCell = 1;
 // -------------------------------------------------------------------
 
-// Định nghĩa lại macro log hoàn chỉnh chống lỗi label
+// Đảm bảo macro log chuẩn xác chống lỗi biên dịch
 #ifndef KELEN_LOG
 #define KELEN_LOG(fmt, ...) NSLog(@"[KelenCore] " fmt, ##__VA_ARGS__)
 #endif
 
-// Khai báo interface đồng bộ mục con với KelenEngine
-@interface KelenAdvancedSubController : PSListController {
-    BOOL _isSyncingCache;
-}
-@property (nonatomic, strong) NSMutableDictionary *masterPreferencesCache;
-- (void)syncStateWithMasterEngine;
+// Khai báo giao diện ẩn cho LaunchServices để lấy danh sách ứng dụng đã cài đặt
+@interface LSApplicationWorkspace : NSObject
++ (id)sharedInstance;
+- (NSArray *)allInstalledApplications;
 @end
 
-@implementation KelenAdvancedSubController
+@interface LSApplicationProxy : NSObject
+@property (nonatomic, readonly) NSString *applicationIdentifier;
+@property (nonatomic, readonly) NSString *localizedName;
+@property (nonatomic, readonly) NSURL *bundleURL;
+@end
 
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        _isSyncingCache = NO;
-        [self syncStateWithMasterEngine];
-    }
-    return self;
+@interface KelenAppSelectorController : PSListController {
+    NSArray *_installedAppsCache;
 }
+@end
+
+@implementation KelenAppSelectorController
 
 - (NSArray *)specifiers {
     if (!_specifiers) {
         NSMutableArray *specs = [NSMutableArray array];
 
-        // --- SECTION 1: CẤU HÌNH MÓC NỐI (HOOK ENGINE) ---
-        PSSpecifier *group1 = [PSSpecifier preferenceSpecifierNamed:@"Cấu hình Mô-đun Hook & Bypass"
-                                                             target:self
-                                                                set:nil
-                                                                get:nil
-                                                             detail:Nil
-                                                               cell:PSGroupCell
-                                                               edit:Nil];
-        [group1 setProperty:@"Đồng bộ trực tiếp trạng thái kiểm soát qua Kelen_MasterSync.h" forKey:@"footerText"];
-        [specs addObject:group1];
+        // Nhóm hướng dẫn
+        PSSpecifier *group = [PSSpecifier preferenceSpecifierNamed:@"Chọn Ứng dụng Mục tiêu Bypass"
+                                                            target:self
+                                                               set:nil
+                                                               get:nil
+                                                            detail:Nil
+                                                              cell:PSGroupCell
+                                                              edit:Nil];
+        [group setProperty:@"Bật công tắc bên cạnh ứng dụng bạn muốn áp dụng cơ chế che giấu Jailbreak cấp thấp." forKey:@"footerText"];
+        [specs addObject:group];
 
-        // Item 1: Anti-Debugging Hook
-        PSSpecifier *item1 = [PSSpecifier preferenceSpecifierNamed:@"Chặn Gỡ lỗi & Phát hiện (Anti-Debug)"
-                                                             target:self
-                                                                set:@selector(setMasterPrefValue:specifier:)
-                                                                get:@selector(getMasterPrefValue:)
-                                                             detail:Nil
-                                                               cell:PSSwitchCell
-                                                               edit:Nil];
-        [item1 setProperty:@"KelenHookAntiDebug" forKey:@"identifier"];
-        [item1 setProperty:@YES forKey:@"default"];
-        [specs addObject:item1];
+        // Lấy danh sách ứng dụng thực tế trên thiết bị
+        @autoreleasepool {
+            Class LSWorkspaceClass = objc_getClass("LSApplicationWorkspace");
+            if (LSWorkspaceClass) {
+                id workspace = [LSWorkspaceClass performSelector:@selector(sharedInstance)];
+                NSArray *apps = [workspace performSelector:@selector(allInstalledApplications)];
+                
+                // Sắp xếp ứng dụng theo tên hiển thị
+                NSArray *sortedApps = [apps sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+                    NSString *name1 = [obj1 performSelector:@selector(localizedName)];
+                    NSString *name2 = [obj2 performSelector:@selector(localizedName)];
+                    return [name1 compare:name2 options:NSCaseInsensitiveSearch];
+                }];
 
-        // Item 2: Dyld Image Hiding
-        PSSpecifier *item2 = [PSSpecifier preferenceSpecifierNamed:@"Ẩn Thư viện Động Dyld & Ellekit"
-                                                             target:self
-                                                                set:@selector(setMasterPrefValue:specifier:)
-                                                                get:@selector(getMasterPrefValue:)
-                                                             detail:Nil
-                                                               cell:PSSwitchCell
-                                                               edit:Nil];
-        [item2 setProperty:@"KelenHookDyld" forKey:@"identifier"];
-        [item2 setProperty:@YES forKey:@"default"];
-        [specs addObject:item2];
-
-        // --- SECTION 2: FILE SYSTEM & KERNEL CHEAT ---
-        PSSpecifier *group2 = [PSSpecifier preferenceSpecifierNamed:@"Ảo hóa Hệ thống Tệp tin (File System)"
-                                                             target:self
-                                                                set:nil
-                                                                get:nil
-                                                             detail:Nil
-                                                               cell:PSGroupCell
-                                                               edit:Nil];
-        [group2 setProperty:@"Che giấu tuyệt đối các đường dẫn rootless và thông tin kernel" forKey:@"footerText"];
-        [specs addObject:group2];
-
-        // Item 3: Rootless Path Hiding
-        PSSpecifier *item3 = [PSSpecifier preferenceSpecifierNamed:@"Ẩn Thư mục Rootless (/var/jb)"
-                                                             target:self
-                                                                set:@selector(setMasterPrefValue:specifier:)
-                                                                get:@selector(getMasterPrefValue:)
-                                                             detail:Nil
-                                                               cell:PSSwitchCell
-                                                               edit:Nil];
-        [item3 setProperty:@"KelenHookFileSystem" forKey:@"identifier"];
-        [item3 setProperty:@YES forKey:@"default"];
-        [specs addObject:item3];
-
-        // Item 4: Sysctl Interception
-        PSSpecifier *item4 = [PSSpecifier preferenceSpecifierNamed:@"Lọc Lệnh Trạng thái Sysctl"
-                                                             target:self
-                                                                set:@selector(setMasterPrefValue:specifier:)
-                                                                get:@selector(getMasterPrefValue:)
-                                                             detail:Nil
-                                                               cell:PSSwitchCell
-                                                               edit:Nil];
-        [item4 setProperty:@"KelenHookSysctl" forKey:@"identifier"];
-        [item4 setProperty:@YES forKey:@"default"];
-        [specs addObject:item4];
-
-        // Item 5: Stock Device Profile Emulation
-        PSSpecifier *item5 = [PSSpecifier preferenceSpecifierNamed:@"Giả lập Hồ sơ Thiết bị Nguyên bản"
-                                                             target:self
-                                                                set:@selector(setMasterPrefValue:specifier:)
-                                                                get:@selector(getMasterPrefValue:)
-                                                             detail:Nil
-                                                               cell:PSSwitchCell
-                                                               edit:Nil];
-        [item5 setProperty:@"KelenStockEmulation" forKey:@"identifier"];
-        [item5 setProperty:@YES forKey:@"default"];
-        [specs addObject:item5];
-
-        // --- SECTION 3: MAINTENANCE ACTIONS ---
-        PSSpecifier *group3 = [PSSpecifier preferenceSpecifierNamed:@"Quản trị & Bảo trì Hệ thống"
-                                                             target:self
-                                                                set:nil
-                                                                get:nil
-                                                             detail:Nil
-                                                               cell:PSGroupCell
-                                                               edit:Nil];
-        [group3 setProperty:@"Các tác vụ dọn dẹp và áp dụng cấu hình nhanh" forKey:@"footerText"];
-        [specs addObject:group3];
-
-        // Item 6: Clear Cache Button
-        PSSpecifier *item6 = [PSSpecifier preferenceSpecifierNamed:@"Xóa Sạch Bộ nhớ Tạm (Cache)"
-                                                             target:self
-                                                                set:nil
-                                                                get:nil
-                                                             detail:nil
-                                                               cell:PSButtonCell
-                                                               edit:nil];
-        [item6 setProperty:NSStringFromSelector(@selector(handleClearCacheAction:)) forKey:@"action"];
-        [specs addObject:item6];
-
-        // Item 7: Respring SpringBoard Button
-        PSSpecifier *item7 = [PSSpecifier preferenceSpecifierNamed:@"Áp dụng & Làm mới (Respring)"
-                                                             target:self
-                                                                set:nil
-                                                                get:nil
-                                                             detail:nil
-                                                               cell:PSButtonCell
-                                                               edit:nil];
-        [item7 setProperty:NSStringFromSelector(@selector(handleRespringAction:)) forKey:@"action"];
-        [specs addObject:item7];
+                for (id app in sortedApps) {
+                    NSString *bundleID = [app performSelector:@selector(applicationIdentifier)];
+                    NSString *appName = [app performSelector:@selector(localizedName)];
+                    
+                    if (bundleID && appName) {
+                        PSSpecifier *appSpec = [PSSpecifier preferenceSpecifierNamed:appName
+                                                                            target:self
+                                                                               set:@selector(setAppActiveState:specifier:)
+                                                                               get:@selector(getAppActiveState:)
+                                                                            detail:Nil
+                                                                              cell:PSSwitchCell
+                                                                              edit:Nil];
+                        [appSpec setProperty:bundleID forKey:@"identifier"];
+                        [appSpec setProperty:bundleID forKey:@"key"];
+                        [specs addObject:appSpec];
+                    }
+                }
+            }
+        }
 
         _specifiers = specs;
     }
     return _specifiers;
 }
 
-- (void)syncStateWithMasterEngine {
-    @autoreleasepool {
-        _isSyncingCache = YES;
-        NSDictionary *loadedPrefs = [NSDictionary dictionaryWithContentsOfFile:KELEN_PREFS_PATH];
-        if (loadedPrefs) {
-            self.masterPreferencesCache = [loadedPrefs mutableCopy];
-        } else {
-            self.masterPreferencesCache = [NSMutableDictionary dictionary];
-        }
-        _isSyncingCache = NO;
-    }
-}
-
-- (id)getMasterPrefValue:(PSSpecifier *)specifier {
-    NSString *key = [specifier propertyForKey:@"identifier"];
-    if (!key) return @YES;
+// Đọc trạng thái kích hoạt riêng cho từng ứng dụng từ file cấu hình trung tâm
+- (id)getAppActiveState:(PSSpecifier *)specifier {
+    NSString *bundleID = [specifier propertyForKey:@"identifier"];
+    if (!bundleID) return @NO;
 
     @autoreleasepool {
-        if (self.masterPreferencesCache[key] != nil) {
-            return self.masterPreferencesCache[key];
-        }
-        
-        NSDictionary *currentPrefs = [NSDictionary dictionaryWithContentsOfFile:KELEN_PREFS_PATH];
-        if (currentPrefs && currentPrefs[key] != nil) {
-            self.masterPreferencesCache[key] = currentPrefs[key];
-            return currentPrefs[key];
+        NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:KELEN_PREFS_PATH];
+        if (prefs && prefs[bundleID]) {
+            return prefs[bundleID];
         }
     }
-    
-    id defaultVal = [specifier propertyForKey:@"default"];
-    return defaultVal ? defaultVal : @YES;
+    return @NO;
 }
 
-- (void)setMasterPrefValue:(id)value specifier:(PSSpecifier *)specifier {
-    if (_isSyncingCache) return;
-    
-    NSString *key = [specifier propertyForKey:@"identifier"];
-    if (!key) return;
+// Ghi trạng thái khi người dùng gạt công tắc cho từng app
+- (void)setAppActiveState:(id)value specifier:(PSSpecifier *)specifier {
+    NSString *bundleID = [specifier propertyForKey:@"identifier"];
+    if (!bundleID) return;
 
     @autoreleasepool {
-        self.masterPreferencesCache[key] = value;
-        
-        NSMutableDictionary *filePrefs = [[NSDictionary dictionaryWithContentsOfFile:KELEN_PREFS_PATH] mutableCopy];
-        if (!filePrefs) {
-            filePrefs = [NSMutableDictionary dictionary];
+        NSMutableDictionary *prefs = [[NSDictionary dictionaryWithContentsOfFile:KELEN_PREFS_PATH] mutableCopy];
+        if (!prefs) {
+            prefs = [NSMutableDictionary dictionary];
         }
-        
-        filePrefs[key] = value;
-        [filePrefs writeToFile:KELEN_PREFS_PATH atomically:YES];
 
-        // Gửi thông báo Darwin qua MasterSync Center
-        CFNotificationCenterRef center = CFNotificationCenterGetDarwinNotifyCenter();
-        CFNotificationCenterPostNotification(center, CFSTR("com.kelen.masterbypass/ReloadPrefs"), NULL, NULL, YES);
-        
-        KELEN_LOG(@"Đã đồng bộ thành công khóa cấu hình: %@ thành giá trị: %@", key, value);
-    }
-}
+        prefs[bundleID] = value;
+        [prefs writeToFile:KELEN_PREFS_PATH atomically:YES];
 
-- (void)handleClearCacheAction:(PSSpecifier *)specifier {
-    @autoreleasepool {
-        KELEN_LOG(@"Thực hiện dọn dẹp bộ nhớ cache đồng bộ...");
+        // Phát tín hiệu đồng bộ Darwin Notification để Tweak trong app mục tiêu nhận diện tức thì
+        CFNotificationCenterPostNotification(
+            CFNotificationCenterGetDarwinNotifyCenter(),
+            CFSTR("com.kelen.masterbypass/ReloadPrefs"),
+            NULL,
+            NULL,
+            YES
+        );
         
-        NSFileManager *fm = [NSFileManager defaultManager];
-        NSString *tmpDir = NSTemporaryDirectory();
-        NSArray *tmpFiles = [fm contentsOfDirectoryAtPath:tmpDir error:nil];
-        for (NSString *file in tmpFiles) {
-            [fm removeItemAtPath:[tmpDir stringByAppendingPathComponent:file] error:nil];
-        }
-        
-        [self.masterPreferencesCache removeAllObjects];
-        [fm removeItemAtPath:KELEN_PREFS_PATH error:nil];
-
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Đã đồng bộ & Dọn dẹp"
-                                                                       message:@"Toàn bộ cache hệ thống và cấu hình tạm đã được làm sạch!"
-                                                                preferredStyle:UIAlertControllerStyleAlert];
-        [alert addAction:[UIAlertAction actionWithTitle:@"Xác nhận" style:UIAlertActionStyleDefault handler:nil]];
-        [self presentViewController:alert animated:YES completion:nil];
-    }
-}
-
-- (void)handleRespringAction:(PSSpecifier *)specifier {
-    @autoreleasepool {
-        KELEN_LOG(@"Tiến hành khởi động lại SpringBoard thông qua tiến trình hệ thống...");
-        
-        pid_t pid;
-        const char *args[] = {"killall", "backboardd", NULL};
-        posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)args, NULL);
+        KELEN_LOG(@"Đã cập nhật trạng thái bypass cho ứng dụng [%@]: %@", bundleID, value);
     }
 }
 
